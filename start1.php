45c48a56dfa42057c990f20f0024858d0c2005d4
Aarti Kashyap <kaarti.sr@gmail.com>
2016-05-21 23:30:25 +0530

    Changing fonts

--- start1.php
+++ start1.php
@@ -3,11 +3,11 @@
 
 <form method="post" action="quiz.php">
 
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-Choose Fandom: <br>
-<input type="radio" name="fandom" value="HP"> Harry Potter<br>
-<input type="radio" name="fandom" value="LOTR"> The Lord of the Rings<br>
+<h1>Name: &nbsp&nbsp<input type="text" name="name"></h1>
+<h1>E-mail: <input type="text" name="email"><br></h1>
+<h1>Choose Fandom: </h1>
+<h4><input type="radio" name="fandom" value="HP"> Harry Potter</h2>
+<h4><input type="radio" name="fandom" value="LOTR"> The Lord of the Rings<br></h2>
 
 <input type="submit" value="Go">
 
